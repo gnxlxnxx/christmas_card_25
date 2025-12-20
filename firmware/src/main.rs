4ab6b9617130b6dab17916e715693fa6f0ff67bd
gnxlxnxx <roman@kretschmer.email>
2025-12-20 16:57:48 +0100

    add ws2812 leds

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -26,6 +26,64 @@ use panic_halt as _;
 
 use crate::matrix::{Button, ButtonPins, Framebuffer, LedPins, Matrix};
 
+pub mod ws2812;
+use crate::ws2812::{Color, HUETABLE, RANDS, SINTABLE};
+
+#[embassy_executor::task(pool_size = 1)]
+async fn ws2812_exec(
+    pin: hal::Peri<'static, hal::peripherals::PC6>,
+    spi1: hal::Peri<'static, hal::peripherals::SPI1>,
+    dma1_ch3: hal::Peri<'static, hal::peripherals::DMA1_CH3>,
+) {
+    let mut ws2812 = ws2812::Ws2812::init(pin, spi1, dma1_ch3);
+
+    let mut phases: [u16; 6] = [0; 6];
+    for i in 0..6 {
+        phases[i] = (i as u16) << 8;
+    }
+
+    let mut desired_output: [Color; 6] = [Color::new(0, 0, 0); 6];
+    let mut output: [Color; 6] = [Color::new(0, 0, 0); 6];
+
+    loop {
+        for k in 0..6 {
+            phases[k] += (((RANDS[k] as u16 + 0xf) << 2) + ((RANDS[k] as u16 + 0xf) << 1)) >> 1;
+        }
+
+        for ledno in 0..6 {
+            // TODO add more modes
+
+            // Original "Fire" mode
+            let index: usize = ((phases[ledno]) >> 8) as usize;
+            let rs: u8 = SINTABLE[index] >> 3;
+
+            desired_output[ledno].r = (HUETABLE[((rs + 30) & 0xff) as usize] as u32 >> 2) as u8;
+            desired_output[ledno].g = (HUETABLE[(rs + 0) as usize] as u32 >> 3) as u8;
+            desired_output[ledno].b = (HUETABLE[((rs + 190) & 0xff) as usize] as u32 >> 3) as u8;
+
+            if output[ledno].r > desired_output[ledno].r {
+                output[ledno].r -= 1;
+            } else if output[ledno].r < desired_output[ledno].r {
+                output[ledno].r += 1;
+            }
+
+            if output[ledno].g > desired_output[ledno].g {
+                output[ledno].g -= 1;
+            } else if output[ledno].g < desired_output[ledno].g {
+                output[ledno].g += 1;
+            }
+
+            if output[ledno].b > desired_output[ledno].b {
+                output[ledno].b -= 1;
+            } else if output[ledno].b < desired_output[ledno].b {
+                output[ledno].b += 1;
+            }
+        }
+        ws2812.start(output).await;
+        Timer::after_millis(30).await;
+    }
+}
+
 #[embassy_executor::main(entry = "ch32_hal::entry")]
 async fn main(spawner: Spawner) -> ! {
     let p = hal::init(hal::Config {
@@ -37,6 +95,10 @@ async fn main(spawner: Spawner) -> ! {
     );
     let btn = ButtonPins::new(p.PD7, p.PD4, p.PC0, p.PD3);
     let matrix = Matrix::init(spawner, led, btn, p.TIM1, p.TIM2);
+
+    spawner
+        .spawn(ws2812_exec(p.PC6.into(), p.SPI1.into(), p.DMA1_CH3.into()))
+        .unwrap();
 
     // Adjust the LED GPIO according to your board
     loop {
