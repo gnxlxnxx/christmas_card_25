764791c698da521d432e9aef41363dc8afaa705e
gnxlxnxx <roman@kretschmer.email>
2025-12-21 00:43:44 +0100

    `rand`om changes

--- firmware/src/drivers/ws2812.rs
+++ firmware/src/drivers/ws2812.rs
@@ -2,6 +2,8 @@ use ch32_hal as hal;
 use hal::spi::{Config, Spi};
 use hal::{Peri, peripherals};
 
+use crate::util::rand;
+
 use embassy_time::Timer;
 
 const BITQUARTETS: [u16; 16] = [
@@ -87,14 +89,15 @@ impl<'a> Ws2812<'a> {
         let mut desired_output: [Color; 6] = [Color::new(0, 0, 0); 6];
 
         match mode {
-            Ws2812Mode::Fire { phases } => {
+            Ws2812Mode::Fire { phases, noisegen } => {
                 // Original "Fire" mode
 
                 Timer::after_millis(30).await;
 
                 for k in 0..6 {
-                    phases[k] +=
-                        (((RANDS[k] as u16 + 0xf) << 2) + ((RANDS[k] as u16 + 0xf) << 1)) >> 1;
+                    phases[k] += (((noisegen.rand8() as u16 + 0xf) << 2)
+                        + ((noisegen.rand8() as u16 + 0xf) << 1))
+                        >> 1;
                 }
 
                 for ledno in 0..6 {
@@ -108,7 +111,11 @@ impl<'a> Ws2812<'a> {
                         (HUETABLE[((rs + 190) & 0xff) as usize] as u32 >> 3) as u8;
                 }
             }
-            Ws2812Mode::Snowball { counter, snowballs } => {
+            Ws2812Mode::Snowball {
+                counter,
+                snowballs,
+                noisegen,
+            } => {
                 // "Snowball" mode
 
                 Timer::after_millis(5).await;
@@ -123,10 +130,10 @@ impl<'a> Ws2812<'a> {
                             .iter()
                             .filter(|snowball| **snowball != Color::new(0, 0, 0))
                             .count() as u8;
-                        let mut hue: u8 = rand8();
+                        let mut hue: u8 = noisegen.rand8();
                         snowballs[0] = snowballs[1];
                         snowballs[1] = snowballs[2];
-                        snowballs[2] = if rand8() % (3 + num_snowballs_r) != 0 {
+                        snowballs[2] = if noisegen.rand8() % (3 + num_snowballs_r) != 0 {
                             Color::new(0, 0, 0)
                         } else {
                             Color::new(
@@ -136,10 +143,10 @@ impl<'a> Ws2812<'a> {
                             )
                         };
 
-                        hue = rand8();
+                        hue = noisegen.rand8();
                         snowballs[5] = snowballs[4];
                         snowballs[4] = snowballs[3];
-                        snowballs[3] = if rand8() % (3 + num_snowballs_l) != 0 {
+                        snowballs[3] = if noisegen.rand8() % (3 + num_snowballs_l) != 0 {
                             Color::new(0, 0, 0)
                         } else {
                             Color::new(
@@ -198,21 +205,31 @@ impl<'a> Ws2812<'a> {
 }
 
 pub enum Ws2812Mode {
-    Fire { phases: [u16; 6] },
-    Snowball { counter: u32, snowballs: [Color; 6] },
-    Huewheel { ws2812_counter: u32 },
+    Fire {
+        phases: [u16; 6],
+        noisegen: rand::WhiteNoiseGenerator,
+    },
+    Snowball {
+        counter: u32,
+        snowballs: [Color; 6],
+        noisegen: rand::WhiteNoiseGenerator,
+    },
+    Huewheel {
+        ws2812_counter: u32,
+    },
 }
 
 impl Ws2812Mode {
     pub fn new() -> Self {
-        Self::new_snowball()
+        Self::new_huewheel()
     }
     pub fn new_fire() -> Self {
         let mut phases: [u16; 6] = [0; 6];
+        let mut noisegen = rand::WhiteNoiseGenerator::new();
         for i in 0..6 {
-            phases[i] = (i as u16) << 8;
+            phases[i] = (noisegen.rand8() as u16) << 7;
         }
-        Self::Fire { phases }
+        Self::Fire { phases, noisegen }
     }
     pub fn new_huewheel() -> Self {
         let ws2812_counter = 0;
@@ -221,7 +238,12 @@ impl Ws2812Mode {
     pub fn new_snowball() -> Self {
         let counter = 0;
         let snowballs: [Color; 6] = [Color::new(0, 0, 0); 6];
-        Self::Snowball { counter, snowballs }
+        let noisegen = rand::WhiteNoiseGenerator::new();
+        Self::Snowball {
+            counter,
+            snowballs,
+            noisegen,
+        }
     }
 }
 
@@ -262,47 +284,3 @@ pub const SINTABLE: [u8; 256] = [
     0x26, 0x28, 0x2a, 0x2d, 0x2f, 0x31, 0x34, 0x36, 0x39, 0x3c, 0x3e, 0x41, 0x44, 0x47, 0x49, 0x4c,
     0x4f, 0x52, 0x55, 0x58, 0x5b, 0x5e, 0x61, 0x64, 0x67, 0x6a, 0x6d, 0x70, 0x73, 0x76, 0x79, 0x7d,
 ];
-
-pub const RANDS: [u8; 256] = [
-    0x67, 0xc6, 0x69, 0x73, 0x51, 0xff, 0x4a, 0xec, 0x29, 0xcd, 0xba, 0xab, 0xf2, 0xfb, 0xe3, 0x46,
-    0x7c, 0xc2, 0x54, 0xf8, 0x1b, 0xe8, 0xe7, 0x8d, 0x76, 0x5a, 0x2e, 0x63, 0x33, 0x9f, 0xc9, 0x9a,
-    0x66, 0x32, 0x0d, 0xb7, 0x31, 0x58, 0xa3, 0x5a, 0x25, 0x5d, 0x05, 0x17, 0x58, 0xe9, 0x5e, 0xd4,
-    0xab, 0xb2, 0xcd, 0xc6, 0x9b, 0xb4, 0x54, 0x11, 0x0e, 0x82, 0x74, 0x41, 0x21, 0x3d, 0xdc, 0x87,
-    0x70, 0xe9, 0x3e, 0xa1, 0x41, 0xe1, 0xfc, 0x67, 0x3e, 0x01, 0x7e, 0x97, 0xea, 0xdc, 0x6b, 0x96,
-    0x8f, 0x38, 0x5c, 0x2a, 0xec, 0xb0, 0x3b, 0xfb, 0x32, 0xaf, 0x3c, 0x54, 0xec, 0x18, 0xdb, 0x5c,
-    0x02, 0x1a, 0xfe, 0x43, 0xfb, 0xfa, 0xaa, 0x3a, 0xfb, 0x29, 0xd1, 0xe6, 0x05, 0x3c, 0x7c, 0x94,
-    0x75, 0xd8, 0xbe, 0x61, 0x89, 0xf9, 0x5c, 0xbb, 0xa8, 0x99, 0x0f, 0x95, 0xb1, 0xeb, 0xf1, 0xb3,
-    0x05, 0xef, 0xf7, 0x00, 0xe9, 0xa1, 0x3a, 0xe5, 0xca, 0x0b, 0xcb, 0xd0, 0x48, 0x47, 0x64, 0xbd,
-    0x1f, 0x23, 0x1e, 0xa8, 0x1c, 0x7b, 0x64, 0xc5, 0x14, 0x73, 0x5a, 0xc5, 0x5e, 0x4b, 0x79, 0x63,
-    0x3b, 0x70, 0x64, 0x24, 0x11, 0x9e, 0x09, 0xdc, 0xaa, 0xd4, 0xac, 0xf2, 0x1b, 0x10, 0xaf, 0x3b,
-    0x33, 0xcd, 0xe3, 0x50, 0x48, 0x47, 0x15, 0x5c, 0xbb, 0x6f, 0x22, 0x19, 0xba, 0x9b, 0x7d, 0xf5,
-    0x0b, 0xe1, 0x1a, 0x1c, 0x7f, 0x23, 0xf8, 0x29, 0xf8, 0xa4, 0x1b, 0x13, 0xb5, 0xca, 0x4e, 0xe8,
-    0x98, 0x32, 0x38, 0xe0, 0x79, 0x4d, 0x3d, 0x34, 0xbc, 0x5f, 0x4e, 0x77, 0xfa, 0xcb, 0x6c, 0x05,
-    0xac, 0x86, 0x21, 0x2b, 0xaa, 0x1a, 0x55, 0xa2, 0xbe, 0x70, 0xb5, 0x73, 0x3b, 0x04, 0x5c, 0xd3,
-    0x36, 0x94, 0xb3, 0xaf, 0xe2, 0xf0, 0xe4, 0x9e, 0x4f, 0x32, 0x15, 0x49, 0xfd, 0x82, 0x4e, 0xa9,
-];
-
-/* White Noise Generator State */
-const NOISE_BITS: u32 = 8;
-const NOISE_MASK: u32 = ((1 << NOISE_BITS) - 1);
-const NOISE_POLY_TAP0: u32 = 31;
-const NOISE_POLY_TAP1: u32 = 21;
-const NOISE_POLY_TAP2: u32 = 1;
-const NOISE_POLY_TAP3: u32 = 0;
-static mut LFSR: u32 = 1;
-
-fn rand8() -> u8 {
-    let mut new_data: u32;
-
-    unsafe {
-        for bit in 0..NOISE_BITS {
-            new_data = ((LFSR >> NOISE_POLY_TAP0)
-                ^ (LFSR >> NOISE_POLY_TAP1)
-                ^ (LFSR >> NOISE_POLY_TAP2)
-                ^ (LFSR >> NOISE_POLY_TAP3));
-            LFSR = (LFSR << 1) | (new_data & 1);
-        }
-
-        (LFSR & NOISE_MASK) as u8
-    }
-}
