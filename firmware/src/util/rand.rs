7a73a5de31ee6f92842d0fcc8149db73d98b129c
gnxlxnxx <roman@kretschmer.email>
2025-12-23 02:02:52 +0100

    use global lfsr
    this makes it at least a little bit more "random"

--- firmware/src/util/rand.rs
+++ firmware/src/util/rand.rs
@@ -1,3 +1,4 @@
+use core::sync::atomic::{AtomicU32, Ordering};
 const NOISE_BITS: u32 = 8;
 const NOISE_MASK: u32 = (1 << NOISE_BITS) - 1;
 const NOISE_POLY_TAP0: u32 = 31;
@@ -5,24 +6,26 @@ const NOISE_POLY_TAP1: u32 = 21;
 const NOISE_POLY_TAP2: u32 = 1;
 const NOISE_POLY_TAP3: u32 = 0;
 
-pub struct WhiteNoiseGenerator {
-    lfsr: u32,
-}
+static LFSR: AtomicU32 = AtomicU32::new(1);
+
+pub struct WhiteNoiseGenerator;
 
 impl WhiteNoiseGenerator {
     pub fn new() -> Self {
-        Self { lfsr: 1 }
+        Self
     }
 
     pub fn rand8(&mut self) -> u8 {
+        let mut lfsr = LFSR.load(Ordering::Relaxed);
         for bit in 0..NOISE_BITS {
-            let new_data: u32 = (self.lfsr >> NOISE_POLY_TAP0)
-                ^ (self.lfsr >> NOISE_POLY_TAP1)
-                ^ (self.lfsr >> NOISE_POLY_TAP2)
-                ^ (self.lfsr >> NOISE_POLY_TAP3);
-            self.lfsr = (self.lfsr << 1) | (new_data & 1);
+            let new_data: u32 = (lfsr >> NOISE_POLY_TAP0)
+                ^ (lfsr >> NOISE_POLY_TAP1)
+                ^ (lfsr >> NOISE_POLY_TAP2)
+                ^ (lfsr >> NOISE_POLY_TAP3);
+            lfsr = (lfsr << 1) | (new_data & 1);
         }
 
-        (self.lfsr & NOISE_MASK) as u8
+        LFSR.store(lfsr, Ordering::Relaxed);
+        (lfsr & NOISE_MASK) as u8
     }
 }
