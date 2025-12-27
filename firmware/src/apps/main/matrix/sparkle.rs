19068c684a03cecfadb794679b7cb4a6e488c985
Lukas Fink <lukas.fink1@gmail.com>
2025-12-27 02:39:27 +0100

    Fix sparkle animation

--- firmware/src/apps/main/matrix/sparkle.rs
+++ firmware/src/apps/main/matrix/sparkle.rs
@@ -1,28 +1,30 @@
 use crate::drivers::matrix::{Framebuffer, Matrix};
 use crate::util::rand;
+use embassy_futures::select::{Either, select};
 use embassy_time::{Duration, Ticker};
 
 pub async fn run() {
-    let mut ticker = Ticker::every(Duration::from_millis(10));
-    let mut counter = 0;
+    let mut update_clock = Ticker::every(Duration::from_millis(4));
+    let mut gen_clock = Ticker::every(Duration::from_millis(256));
     let mut noisegen = rand::WhiteNoiseGenerator::new();
 
     loop {
-        for row in 0..Framebuffer::HEIGHT {
-            for col in 0..Framebuffer::WIDTH {
-                let cur: u8 = Matrix::fb().load(col, row);
-                Matrix::fb().store(col, row, ((cur << 7) - cur) >> 7);
+        match select(gen_clock.next(), update_clock.next()).await {
+            Either::First(()) => {
+                if noisegen.rand8() < 128 {
+                    let row = noisegen.rand8() as usize % Framebuffer::HEIGHT;
+                    let col = noisegen.rand8() as usize % Framebuffer::WIDTH;
+                    Matrix::fb().store(col, row, 255);
+                }
+            }
+            Either::Second(()) => {
+                for row in 0..Framebuffer::HEIGHT {
+                    for col in 0..Framebuffer::WIDTH {
+                        let cur = Matrix::fb().load(col, row) as u32;
+                        Matrix::fb().store(col, row, (((cur << 7) - cur) >> 7) as u8);
+                    }
+                }
             }
         }
-        if counter >= 0x20 {
-            if noisegen.rand8() < 128 {
-                let row = noisegen.rand8() as usize % Framebuffer::HEIGHT;
-                let col = noisegen.rand8() as usize % Framebuffer::WIDTH;
-                Matrix::fb().store(col, row, 255);
-            }
-            counter = 0;
-        }
-        counter += 1;
-        ticker.next().await;
     }
 }
