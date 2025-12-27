6efdb624620cced7967d345f05bcc7ca6bb06297
Lukas Fink <lukas.fink1@gmail.com>
2025-12-27 02:08:47 +0100

    Optimize matrix animations

--- firmware/src/apps/main/matrix/message.rs
+++ firmware/src/apps/main/matrix/message.rs
@@ -4,10 +4,12 @@ use crate::util::text;
 
 const MESSAGE: &[u8] = b"Die Fachschaft Elektro- und Informationstechnik an der \x80 Universit\xE4t Stuttgart w\xFCnscht Euch allen recht herzlich ein frohes Weihnachtsfest!";
 
-pub async fn run() {
+pub async fn run() -> ! {
     let mut clock = Ticker::every(Duration::from_millis(50));
 
-    text::clear_scroll(&mut clock).await;
-    text::scroll(MESSAGE, 64, &mut clock).await;
-    text::clear_scroll(&mut clock).await;
+    loop {
+        text::clear_scroll(&mut clock).await;
+        text::scroll(MESSAGE, 64, &mut clock).await;
+        text::clear_scroll(&mut clock).await;
+    }
 }
--- firmware/src/apps/main/matrix/rand_pulse.rs
+++ firmware/src/apps/main/matrix/rand_pulse.rs
@@ -1,34 +1,35 @@
 use crate::drivers::matrix::{Framebuffer, Matrix};
 use crate::util::rand;
+use embassy_futures::select::{Either, select};
 use embassy_time::{Duration, Ticker};
 
 pub async fn run() -> ! {
-    let mut ticker = Ticker::every(Duration::from_millis(10));
-    let mut counter = 0;
+    let mut update_clock = Ticker::every(Duration::from_millis(10));
+    let mut gen_clock = Ticker::every(Duration::from_millis(300));
     let mut buffer_matrix = [[0; Framebuffer::WIDTH]; Framebuffer::HEIGHT];
     let mut noisegen = rand::WhiteNoiseGenerator::new();
 
     loop {
-        if counter >= 30 {
-            let row = noisegen.rand8() as usize % Framebuffer::HEIGHT;
-            let col = noisegen.rand8() as usize % Framebuffer::WIDTH;
-            buffer_matrix[row][col] = 150;
-            counter = 0;
-        }
-        counter += 1;
-
-        for (row, buffer_row) in buffer_matrix.iter_mut().enumerate().take(Framebuffer::HEIGHT) {
-            for (col, buffer_field) in buffer_row.iter_mut().enumerate().take(Framebuffer::WIDTH) {
-                if *buffer_field < Matrix::fb().load(col, row) {
-                    Matrix::fb().store(col, row, Matrix::fb().load(col, row) - 1);
-                } else if *buffer_field > Matrix::fb().load(col, row) {
-                    Matrix::fb().store(col, row, Matrix::fb().load(col, row) + 1);
-                }
-                if *buffer_field == Matrix::fb().load(col, row) {
-                    *buffer_field = 0;
+        match select(gen_clock.next(), update_clock.next()).await {
+            Either::First(()) => {
+                let row = noisegen.rand8() as usize % Framebuffer::HEIGHT;
+                let col = noisegen.rand8() as usize % Framebuffer::WIDTH;
+                buffer_matrix[row][col] = 150;
+            }
+            Either::Second(()) => {
+                for (row, buffer_row) in buffer_matrix.iter_mut().enumerate().take(Framebuffer::HEIGHT) {
+                    for (col, buffer_field) in buffer_row.iter_mut().enumerate().take(Framebuffer::WIDTH) {
+                        if *buffer_field < Matrix::fb().load(col, row) {
+                            Matrix::fb().store(col, row, Matrix::fb().load(col, row) - 1);
+                        } else if *buffer_field > Matrix::fb().load(col, row) {
+                            Matrix::fb().store(col, row, Matrix::fb().load(col, row) + 1);
+                        }
+                        if *buffer_field == Matrix::fb().load(col, row) {
+                            *buffer_field = 0;
+                        }
+                    }
                 }
             }
         }
-        ticker.next().await;
     }
 }
