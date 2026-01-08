4b5c512d4393c8adcd1c18d978590c247ca32f3d
Lukas Fink <lukas.fink1@gmail.com>
2026-01-09 00:27:31 +0100

    Reduce size of show_score

--- firmware/src/apps/games/mod.rs
+++ firmware/src/apps/games/mod.rs
@@ -3,37 +3,31 @@ use embassy_time::Ticker;
 
 use crate::{drivers::{buttons::{Button, Buttons, Event}, ws2812::Ws2812}, util::{itoa::utoa10, text::{self, TEXT_BRIGHTNESS, TEXT_DURATION}}};
 
+const HIGH_SCORE_TEXT: &[u8; 13] = b" High Score: ";
+
 pub mod snake;
 pub mod tetris;
 
 async fn show_score(has_won: bool, score: u32, high_score: u32) {
     let mut clock = Ticker::every(TEXT_DURATION);
+    let scroll = if has_won {
+        text::scroll(b"Herzlichen Gl\x82ckwunsch!", 2 * TEXT_BRIGHTNESS, &mut clock)
+    } else {
+        text::scroll(b"Game Over!", TEXT_BRIGHTNESS, &mut clock)
+    };
+
+    select(wait_for_start_or_select(), scroll).await;
 
     select(
         wait_for_start_or_select(),
         async {
-            if has_won {
-                text::scroll(b"Herzlichen Gl\x82ckwunsch!", 2 * TEXT_BRIGHTNESS, &mut clock).await;
-            } else {
-                text::scroll(b"Game Over!", TEXT_BRIGHTNESS, &mut clock).await;
-            }
-        }
-    ).await;
-
-    select(
-        wait_for_start_or_select(),
-        async {
-            let mut itoa_score = [0u8; 10];
-            let mut itoa_hs = [0u8; 10];
-
-            let score_str = utoa10(score, &mut itoa_score);
-            let hs_str = utoa10(high_score, &mut itoa_hs);
+            let mut itoa_buf = [0u8; 10];
 
             loop {
-                text::scroll(b" Score: ", TEXT_BRIGHTNESS, &mut clock).await;
-                text::scroll(score_str, TEXT_BRIGHTNESS, &mut clock).await;
-                text::scroll(b" High Score: ", TEXT_BRIGHTNESS, &mut clock).await;
-                text::scroll(hs_str, TEXT_BRIGHTNESS, &mut clock).await;
+                text::scroll(&HIGH_SCORE_TEXT[5..], TEXT_BRIGHTNESS, &mut clock).await;
+                text::scroll(utoa10(score, &mut itoa_buf), TEXT_BRIGHTNESS, &mut clock).await;
+                text::scroll(HIGH_SCORE_TEXT, TEXT_BRIGHTNESS, &mut clock).await;
+                text::scroll(utoa10(high_score, &mut itoa_buf), TEXT_BRIGHTNESS, &mut clock).await;
             }
         }
     ).await;
