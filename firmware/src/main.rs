df25b53830eea6e3d412f23cd87ae2e33f015d9a
Lukas Fink <lukas.fink1@gmail.com>
2026-01-21 19:31:16 +0100

    Manually initialize clocks

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -8,8 +8,9 @@ pub mod drivers;
 pub mod util;
 mod vectors;
 
-use ch32_hal::interrupt::InterruptExt;
 use ch32_hal::{self as hal};
+use qingke::{interrupt::Priority, pfic};
+use ch32_metapac::{self as pac, Interrupt, rcc::vals::Sw};
 use embassy_futures::block_on;
 use panic_halt as _;
 
@@ -21,14 +22,41 @@ use drivers::{
 use crate::apps::{games, main};
 use crate::util::ws2812::FilteredWs2812;
 
+fn init_hsi_pll_48mhz() {
+    pac::RCC.ctlr().write(|w| {
+        w.set_hsitrim(16);
+        w.set_hsion(true);
+        w.set_pllon(true);
+    });
+    while !pac::RCC.ctlr().read().pllrdy() {}
+    pac::FLASH.actlr().write(|w| w.set_latency(1));
+    pac::RCC.cfgr0().write(|w| w.set_sw(Sw::PLL));
+    while pac::RCC.cfgr0().read().sws() != Sw::PLL {}
+}
+
 #[qingke_rt::entry]
 fn main() -> ! {
-    let p = hal::init(hal::Config {
-        rcc: hal::rcc::Config::SYSCLK_FREQ_48MHZ_HSI,
+    init_hsi_pll_48mhz();
+
+    // Enable peripherals
+    pac::RCC.apb2pcenr().write(|w| {
+        w.set_spi1en(true);
+        w.set_tim1en(true);
+        w.set_iopden(true);
+        w.set_iopcen(true);
+        w.set_iopaen(true);
+        w.set_afioen(true);
+    });
+    pac::RCC.apb1pcenr().write(|w| {
+        w.set_tim2en(true)
     });
 
-    hal::interrupt::EXTI7_0.set_priority(hal::interrupt::Priority::P0);
-    hal::interrupt::TIM1_UP.set_priority(hal::interrupt::Priority::P8);
+    unsafe {
+        pfic::set_priority(Interrupt::EXTI7_0 as u8, Priority::P0.into());
+        pfic::set_priority(Interrupt::TIM1_UP as u8, Priority::P8.into());
+    }
+
+    let p = unsafe { hal::Peripherals::steal() };
 
     let led = matrix::Pins::new(
         p.PD0, p.PA2, p.PA1, p.PD6, p.PD5, p.PD2, p.PC7, p.PC4, p.PC1,
